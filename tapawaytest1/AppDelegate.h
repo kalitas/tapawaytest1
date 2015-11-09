14424c52680f868d542be0a2799f0e2e6843e102
dan goldberg <kalitas_g@hotmail.com>
2015-11-09 10:54:48 +0200

    Initial Commit

---
+++ tapawaytest1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tapawaytest1
+//
+//  Created by dan g / applicaster on 11/9/15.
+//  Copyright © 2015 tapaway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tapawaytest1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tapawaytest1
+//
+//  Created by dan g / applicaster on 11/9/15.
+//  Copyright © 2015 tapaway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
